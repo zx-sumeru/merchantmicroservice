82c64416740ab40bdafde0c48b025823c9fb3ba4
Zhang Xu <1243399327@qq.com>
2018-08-26 23:41:01 +0800

    Initial commit

---
+++ pom-demo/src/main/java/com/example/pomdemo/PomDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.pomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomDemoApplication.class, args);
+    }
+}
